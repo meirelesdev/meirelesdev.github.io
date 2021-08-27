92408d8f9990b68d45c0999f376abe88bbda16e9
Daniel Meireles do Nascimento <daniel@inovadora.com.br>
2021-08-27 17:53:34 -0300

    Ajustes css e js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,15 @@
+const toggle = () => {
+    let menuMobile = document.getElementById("menu-mobile")
+    if (menuMobile.classList.contains('show')) {
+        menuMobile.classList.add('hidden')
+        menuMobile.classList.remove('show')
+    } else {
+        menuMobile.classList.add('show')
+        menuMobile.classList.remove('hidden')
+    }
+}
+let btnClose = document.getElementById('btn-close')
+let btnOpen = document.getElementById('btn-open')
 
-//  inicio do codigo para o menu mobile
-
-    let btnClose = document.getElementById('btn-close')
-    let btnOpen = document.getElementById('btn-open')
-
-    
-    btnOpen.addEventListener('click', toggle,false)
-    btnClose.addEventListener('click', toggle,false)
-    // função para mostrar e esconder o menu-mobile
-    function toggle() {
-        
-        let menuMobile = document.getElementById("menu-mobile")
-        
-        if(menuMobile.classList.contains('show')){
-            menuMobile.classList.add('hidden')
-            menuMobile.classList.remove('show')
-        }else{
-            menuMobile.classList.add('show')
-            menuMobile.classList.remove('hidden')
-        }
-        
-    }    
-
-//  fim do cobido para o menu mobile
+btnOpen.addEventListener('click', toggle, false)
+btnClose.addEventListener('click', toggle, false)
